6db1e2ffb33b4b495fdf2fffa2b0afe0477621ba
Tiffany Zhang <tzhang@lcs.on.ca>
2024-12-06 14:43:33 -0500

    Added a title to the World Clock View

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 struct WorldClockView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            Text("World Clock")
+                .navigationTitle("World Clock")
+        }
     }
+    
+    
 }
 
 #Preview {
-    WorldClockView()
+    LandingView()
 }
