63a0b7dd8cbeb6e391d2b52eef3fe723fbaca1c7
Tiffany Zhang <tzhang@lcs.on.ca>
2024-12-06 14:26:05 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by xinyu zhang on 2024-12-06.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
